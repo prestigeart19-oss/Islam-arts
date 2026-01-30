7ba517b360dcdc8fd821464814c00ae402c93999
prestigeart19-oss <prestigeart19@gmail.com>
2026-01-30 21:41:13 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'islam-arts-v1';
+const urlsToCache = [
+  '/islamic_quiz_game.html',
+  '/'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retourne le cache si disponible, sinon fetch
+        return response || fetch(event.request);
+      })
+  );
+});
